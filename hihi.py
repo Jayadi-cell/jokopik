9f1e517ec80746f02ac9f364b2188a89e68e4ff5
Jayadi-cell <aljaymimajid@gmail.com>
2026-09-04 11:16:29 +0800

    update ke enam in hihi.py

--- hihi.py
+++ hihi.py
@@ -1,7 +1,7 @@
 import random
 
 print("SELAMAT DATANG DI TEBAK KAMAR KUCING")
-print("SELAMAT BERJUANG SUKSES")
+print("PAK JOKOWI BILANG SELAMAT BERJUANG SUKSES")
 posisi_kucing = random.randint(1,5)
 
 nama_pemain = input("masukan nama anda:")
